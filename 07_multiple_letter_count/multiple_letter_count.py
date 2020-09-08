5d1b8720c0f36c3916b76790d083b414084e175d
Marina8989 <marina.mill209@gmail.com>
2020-09-08 15:22:43 -0700

    python practice

--- 07_multiple_letter_count/multiple_letter_count.py
+++ 07_multiple_letter_count/multiple_letter_count.py
@@ -7,3 +7,17 @@ def multiple_letter_count(phrase):
         >>> multiple_letter_count('Yay')
         {'Y': 1, 'a': 1, 'y': 1}
     """
+
+    dict = {}
+
+    for n in phrase:
+       key = dict.keys()
+       if n in key:
+           dict[n] += 1
+       else:
+           dict[n] = 1
+    return dict
+
+
+print(multiple_letter_count('yay'))
+print(multiple_letter_count('Yay'))
