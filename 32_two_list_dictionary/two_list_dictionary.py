35fbc0db2ec16edc596031b84b15f7f55fd59ed0
Marina8989 <marina.mill209@gmail.com>
2020-09-10 16:12:05 -0700

    python practice

--- 32_two_list_dictionary/two_list_dictionary.py
+++ 32_two_list_dictionary/two_list_dictionary.py
@@ -15,3 +15,8 @@ def two_list_dictionary(keys, values):
         >>> two_list_dictionary(['a', 'b', 'c'], [1, 2, 3, 4])
         {'a': 1, 'b': 2, 'c': 3}
    """
+
+
+print(two_list_dictionary(['x', 'y', 'z'], [9, 8, 7]))
+print(two_list_dictionary(['a', 'b', 'c', 'd'], [1, 2, 3]))
+print(two_list_dictionary(['a', 'b', 'c'], [1, 2, 3, 4]))
