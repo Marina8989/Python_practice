096dbc1dac5bbe1ff643688132b5173717a17456
Marina8989 <marina.mill209@gmail.com>
2020-09-10 14:18:38 -0700

    python practice

--- 28_find_factors/find_factors.py
+++ 28_find_factors/find_factors.py
@@ -13,3 +13,24 @@ def find_factors(num):
     >>> find_factors(321421)
     [1, 293, 1097, 321421]
     """
+    n_list = [n for n in range(1, num // 2 + 1) if num % n == 0]
+
+    n_list.append(num)
+
+    return n_list
+
+
+    # fact = []
+    #
+    # while(n <= num):
+    #     if num % n == 0:
+    #         fact.append(n)
+    #     n += 1
+    #
+    # return fact
+
+
+print(find_factors(10))
+print(find_factors(11))
+print(find_factors(111))
+print(find_factors(321421))
