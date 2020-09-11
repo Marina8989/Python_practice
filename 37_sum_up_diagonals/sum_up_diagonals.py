333bdfb0349992fb80bbbe94c5f9c9974ee6735f
Marina8989 <marina.mill209@gmail.com>
2020-09-10 20:33:24 -0700

    python practice

--- 37_sum_up_diagonals/sum_up_diagonals.py
+++ 37_sum_up_diagonals/sum_up_diagonals.py
@@ -1,3 +1,14 @@
+m1 = [
+    [1,   2],
+    [30, 40],
+]
+
+m2 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+
 def sum_up_diagonals(matrix):
     """Given a matrix [square list of lists], return sum of diagonals.
 
@@ -18,3 +29,13 @@ def sum_up_diagonals(matrix):
         >>> sum_up_diagonals(m2)
         30
     """
+    total = 0
+
+    for i in range(len(matrix)):
+       total += matrix[i][i]
+       total += matrix[i][-1 - i]
+    return total
+
+
+print(sum_up_diagonals(m1))
+print(sum_up_diagonals(m2))
